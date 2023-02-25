289c0dbfc5f32c2fc38777996d3dc01ef84b61c9
Pedro Franca <pedrodepaulafranca@gmail.com>
2023-02-25 18:25:09 -0300

    feat: add scrape_news function to create a dict with the relevant info from each news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -40,7 +40,29 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news_url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author > a::text").get()
+    reading_time = selector.css(".meta-reading-time::text").re_first(r"\d+")
+
+    summary = selector.css(
+        ".entry-content > p:nth-of-type(1) *::text"
+    ).getall()
+    summary = "".join(summary).strip()
+
+    category = selector.css(".label::text").get()
+
+    return {
+        "url": news_url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(reading_time),
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
