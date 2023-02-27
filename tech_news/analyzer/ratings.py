b24c82252464e7c2f32b8c0ac61be8657718a93d
Pedro Franca <pedrodepaulafranca@gmail.com>
2023-02-27 16:24:16 -0300

    feat: adds function top_5_categories to show the 5 most common categories in the scraped news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,19 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    raw_categories_list = [news["category"] for news in news_list]
+
+    if not raw_categories_list:
+        return []
+
+    raw_categories_list.sort()
+
+    sorted_categories_list = [
+        category[0] for category in Counter(raw_categories_list).most_common(5)
+    ]
+
+    return sorted_categories_list
