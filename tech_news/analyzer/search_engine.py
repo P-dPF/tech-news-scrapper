8c6e3e8a482a4ef39794441f6d7e7c4aa718ff3a
Pedro Franca <pedrodepaulafranca@gmail.com>
2023-02-27 13:58:00 -0300

    feat: adds search_by_title funtion to search news by their titles case insensitively

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,19 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    search_result = search_news(query)
+    formatted_results = list()
+
+    if not search_result:
+        return []
+
+    for result in search_result:
+        formatted_results.append((result["title"], result["url"]))
+
+    return formatted_results
 
 
 # Requisito 8
