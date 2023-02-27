76d83d34974654fa556306d0e9dd98d19ceab322
Pedro Franca <pedrodepaulafranca@gmail.com>
2023-02-27 17:49:54 -0300

    feat: adds function to search news by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -18,7 +19,29 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    received_date_format = "%Y-%m-%d"
+    try:
+        valid_date = datetime.strptime(date, received_date_format).date()
+
+        if valid_date:
+            date_elements = str(valid_date).split("-")
+            year, month, day = (
+                date_elements[0],
+                date_elements[1],
+                date_elements[2],
+            )
+
+            query = {"timestamp": f"{day}/{month}/{year}"}
+            search_result = search_news(query)
+
+            formatted_results = list()
+            for result in search_result:
+                formatted_results.append((result["title"], result["url"]))
+
+            return formatted_results
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
